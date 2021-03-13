5ef27d126bd47bd1b6ad94c2a618d46b974c5ddc
Thomas <tvle83@gmail.com>
2021-03-13 10:27:39 -0600

    registered domains for testing.

--- scripts/launch/10_reg_domain.sh
+++ scripts/launch/10_reg_domain.sh
@@ -8,4 +8,11 @@
 
 ./clio -u http://localhost:8889 push action -j fio.address regdomain '{"fio_domain":"faucet","owner_fio_public_key":"","max_fee":"40000000000","actor":"qhh25sqpktwh","tpid":""}' --permission qhh25sqpktwh@active
 ./clio -u http://localhost:8889 push action -j fio.address setdomainpub '{"fio_domain":"faucet","is_public":1,"max_fee":"40000000000","actor":"qhh25sqpktwh","tpid":""}' --permission qhh25sqpktwh@active
+
+./clio -u http://localhost:8889 push action -j fio.address regdomain '{"fio_domain":"awesome","owner_fio_public_key":"","max_fee":"40000000000","actor":"wjeo4abnk4c2","tpid":""}' --permission wjeo4abnk4c2@active
+./clio -u http://localhost:8889 push action -j fio.address regdomain '{"fio_domain":"thomas","owner_fio_public_key":"","max_fee":"40000000000","actor":"wjeo4abnk4c2","tpid":""}' --permission wjeo4abnk4c2@active
+./clio -u http://localhost:8889 push action -j fio.address regdomain '{"fio_domain":"blah","owner_fio_public_key":"","max_fee":"40000000000","actor":"wjeo4abnk4c2","tpid":""}' --permission wjeo4abnk4c2@active
+./clio -u http://localhost:8889 push action -j fio.address regdomain '{"fio_domain":"doctor","owner_fio_public_key":"","max_fee":"40000000000","actor":"wjeo4abnk4c2","tpid":""}' --permission wjeo4abnk4c2@active
+./clio -u http://localhost:8889 push action -j fio.address regdomain '{"fio_domain":"anchor","owner_fio_public_key":"","max_fee":"40000000000","actor":"wjeo4abnk4c2","tpid":""}' --permission wjeo4abnk4c2@active
+
 sleep 0.5
