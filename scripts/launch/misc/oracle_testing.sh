965e95f5768f015e89e67913d1110b9dac5a33d6
Casey Gardiner <casey@dapix.io>
2021-06-30 14:37:48 -0600

    Add regoracle calls

---
+++ scripts/launch/misc/oracle_testing.sh
@@ -0,0 +1,4 @@
+
+./clio -u https://localhost:8889 push action fio.oracle regoracle '{"oracle_actor":"qbxn5zhw2ypw","actor":"eosio"}' --permission eosio
+./clio -u https://localhost:8889 push action fio.oracle regoracle '{"oracle_actor":"hfdg2qumuvlc","actor":"eosio"}' --permission eosio
+./clio -u https://localhost:8889 push action fio.oracle regoracle '{"oracle_actor":"wttywsmdmfew","actor":"eosio"}' --permission eosio
