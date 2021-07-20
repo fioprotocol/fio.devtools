e0a98593723af96ee44b61ad2f245cc26d2def58
Casey Gardiner <casey@dapix.io>
2021-07-20 11:06:05 -0600

    Add domian wrapping actions

--- scripts/launch/18_create_set_fees_after_genesis.sh
+++ scripts/launch/18_create_set_fees_after_genesis.sh
@@ -38,3 +38,4 @@
 ./clio -u http://localhost:8879 push action -f fio.fee createfee '{"end_point":"msig_invalidate","type":"0","suf_amount":"400000000"}' --permission fio.fee@active
 
 ./clio -u http://localhost:8879 push action -f fio.fee createfee '{"end_point":"wrap_fio_tokens","type":"0","suf_amount":"400000000"}' --permission fio.fee@active
+./clio -u http://localhost:8879 push action -f fio.fee createfee '{"end_point":"wrap_fio_domain","type":"0","suf_amount":"400000000"}' --permission fio.fee@active
