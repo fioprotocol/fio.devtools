67a0d54ba116bd647de61b2b6c4a0ae187884af7
Michael Leet <michael@fioprotocol.io>
2022-11-11 16:00:40 -0700

    BD-4105: Added text during execution for clarity, added exclusion options during hash compare, cleaned up bash hash output, removed deprecated hash option

--- scripts/queries/general.sh
+++ scripts/queries/general.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ -z "$1" ]; then
-    read -p $'\n1. Tables 2. Contract ABI/WASM Hashes 3. Generate ABI/WASM Hashes 4. Contract Hashes (deprecated)\nChoose(#):' mChoice
+    read -p $'\n1. Tables 2. Compare ABI/WASM Hashes 3. Output ABI/WASM Hashes\nChoose(#):' mChoice
 else
     mChoice=$1
 fi
@@ -13,10 +13,7 @@ elif [ $mChoice == 2 ]; then
     scripts/queries/abi_wasm_hashout.sh -c
     exit
 elif [ $mChoice == 3 ]; then
-    scripts/queries/abi_wasm_hashout.sh -n
-    exit
-elif [ $mChoice == 4 ]; then
-    scripts/queries/hashes.sh
+    scripts/queries/abi_wasm_hashout.sh -o
     exit
 else
     echo "Unknown option: $mChoice! Exiting..."
