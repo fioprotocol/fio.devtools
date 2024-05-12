d953c96e8f8f07e1b75cf955b4ce1355118399d1
Michael Leet <michael@fioprotocol.io>
2024-05-12 11:54:37 -0600

    BD-4617: Updated local chain startup to use pre-built 2.5.0 base contracts, env cleanup,  minor format cleanup

--- scripts/launch/04_create_accounts.sh
+++ scripts/launch/04_create_accounts.sh
@@ -30,7 +30,7 @@ sleep 1.5
 sleep 10
 ./clio -u http://localhost:8879 set contract eosio.msig $eosio_msig_contract_base_path eosio.msig.wasm eosio.msig.abi
 
-./clio -u http://localhost:8879 set contract -j fio.address $fio_contract_base_path fio.address.wasm fio.address.abi --permission fio.address@active
+./clio -u http://localhost:8879 set contract -j fio.address $fio_addr_contract_base_path fio.address.wasm fio.address.abi --permission fio.address@active
 sleep 2s
 
 echo creating dev account for escrow and marketplace
@@ -40,5 +40,5 @@ echo creating dev account for escrow and marketplace
 #FIO Internal Account (actor name): i4wplnm2ogsd
 
 ./clio -u http://localhost:8879 create account eosio fio.oracle FIO7isxEua78KPVbGzKemH4nj2bWE52gqj8Hkac3tc7jKNvpfWzYS FIO7isxEua78KPVbGzKemH4nj2bWE52gqj8Hkac3tc7jKNvpfWzYS
-./clio -u http://localhost:8879 set contract fio.address $fio_contract_base_path fio.address.wasm fio.address.abi --permission fio.address@active
+./clio -u http://localhost:8879 set contract fio.address $fio_addr_contract_base_path fio.address.wasm fio.address.abi --permission fio.address@active
 sleep 2
